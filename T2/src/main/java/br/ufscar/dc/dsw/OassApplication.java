920530cb7123107271083bc8df641121b3a1b305
Laysson Santos <laysson.nvt@gmail.com>
2024-08-22 17:02:05 -0300

    CRUD Clientes
    Ajuste para testar o CRUD dos Clientes

--- T2/src/main/java/br/ufscar/dc/dsw/OassApplication.java
+++ T2/src/main/java/br/ufscar/dc/dsw/OassApplication.java
@@ -1,13 +1,58 @@
 package br.ufscar.dc.dsw;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.text.SimpleDateFormat;
+
+import br.ufscar.dc.dsw.dao.IClienteDAO;
+import br.ufscar.dc.dsw.domain.Cliente;
 
 @SpringBootApplication
 public class OassApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OassApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(IClienteDAO clienteDAO) {
+		return (args) -> {
+
+			// Criando clientes
+			// Exemplo 1
+			Cliente cliente1 = new Cliente();
+			cliente1.setNome("Maria Oliveira");
+			cliente1.setEmail("maria.oliveira@example.com");
+			cliente1.setCPF("123.456.789-00");
+			cliente1.setTelefone("(11) 98765-4321");
+			cliente1.setSexo("Feminino");
+			cliente1.setDataNascimento(new SimpleDateFormat("dd/MM/yyyy").parse("15/03/1985"));
+			clienteDAO.save(cliente1);
+
+			// Exemplo 2
+			Cliente cliente2 = new Cliente();
+			cliente2.setNome("João Santos");
+			cliente2.setEmail("joao.santos@example.com");
+			cliente2.setCPF("987.654.321-00");
+			cliente2.setTelefone("(21) 91234-5678");
+			cliente2.setSexo("Masculino");
+			cliente2.setDataNascimento(new SimpleDateFormat("dd/MM/yyyy").parse("22/07/1990"));
+			clienteDAO.save(cliente2);
+
+			// Exemplo 3
+			Cliente cliente3 = new Cliente();
+			cliente3.setNome("Ana Souza");
+			cliente3.setEmail("ana.souza@example.com");
+			cliente3.setCPF("456.789.123-00");
+			cliente3.setTelefone("(31) 99876-5432");
+			cliente3.setSexo("Feminino");
+			cliente3.setDataNascimento(new SimpleDateFormat("dd/MM/yyyy").parse("08/11/1988"));
+			clienteDAO.save(cliente3);
+
+		};
 	}
 
 }
--- T2/src/main/java/br/ufscar/dc/dsw/domain/Cliente.java
+++ T2/src/main/java/br/ufscar/dc/dsw/domain/Cliente.java
@@ -1,53 +1,61 @@
 package br.ufscar.dc.dsw.domain;
 
 import java.util.Date;
-import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import br.ufscar.dc.dsw.validation.UniqueCPF;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Past;
 
+@SuppressWarnings("serial")
 @Entity
-@Table(name = "clientes")
+@Table(name = "Cliente")
 public class Cliente extends AbstractEntity<Long> {
 
-    @NotBlank(message = "{NotBlank.aluno.email}")
-    @Email(message = "{Email.aluno.email}")
+    @NotBlank(message = "{NotBlank.cliente.email}")
+    @Email
     @Size(max = 60)
     @Column(nullable = false, unique = true, length = 60)
     private String email;
 
-    @UniqueCPF (message = "{Unique.aluno.CPF}")
-    @NotBlank(message = "{NotBlank.aluno.cpf}")
-    @Size(min = 14, max = 14, message = "{Size.aluno.cpf}")
+    @UniqueCPF (message = "{Unique.cliente.CPF}")
+    @NotBlank(message = "{NotBlank.cliente.cpf}")
+    @Size(min = 14, max = 14, message = "{Size.cliente.cpf}")
     @Column(nullable = false, unique = true, length = 14)
     private String CPF;
 
-    @NotBlank(message = "{NotBlank.aluno.nome}")
+    @NotBlank(message = "{NotBlank.cliente.nome}")
     @Size(max = 60)
     @Column(nullable = false, length = 60)
     private String nome;
 
-    @NotBlank
-    @Size(min = 15, max = 15, message = "{Size.aluno.telefone}")
+    @NotBlank(message = "{NotBlank.cliente.telefone}")
+    @Size(min = 15, max = 15, message = "{Size.cliente.telefone}")
     @Column(nullable = false, length = 60)
     private String telefone;
 
-    @NotBlank(message = "{NotBlank.aluno.sexo}")
+    @NotBlank(message = "{NotBlank.cliente.sexo}")
     @Size(max = 10)
     @Column(nullable = false, length = 10)
     private String sexo;
 
-    @Column(name = "data_nascimento")
+    @Basic
+    @NotNull(message = "{NotNull.cliente.data}")
+    @Past(message = "A data de nascimento deve estar no passado.")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "dataNascimento", nullable = false)
     private Date dataNascimento;
-
-    @OneToMany(mappedBy = "cliente")
-    private List<Consulta> consultas;
 
     public String getEmail() {
         return email;
@@ -95,13 +103,5 @@ public class Cliente extends AbstractEntity<Long> {
 
     public void setDataNascimento(Date dataNascimento) {
         this.dataNascimento = dataNascimento;
-    }
-
-    public List<Consulta> getConsultas() {
-        return consultas;
-    }
-
-    public void setConsultas(List<Consulta> consultas) {
-        this.consultas = consultas;
     }
 }
